fd32597e8edcc9244bcee5179fa2d329f6155270
Stepan <El-Sedrick927@mail.ru>
2025-05-02 10:48:23 +0300

    доделал 7 шаг

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,57 @@
 import React from 'react';
-import './App.css'
 import styles from './App.css';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 export const App = () => {
+	const [value, setValue] = useState('');
+	// const [list , setList] = useState([]);
+	const [error, setError] = useState('');
 
-  return (
-  <div className= {styles.app}>
-    <h1 className={styles['page-heading']}>Ввод значения</h1>
-    <p className={styles['no-margin-text']}>
-      Текущее значение <code>value</code>: "<output className={styles['current-value']}></output>"
-    </p>
-    <div className={styles.error}> Введенное значение должно содержать минимум 3 символа </div>
-    <div className={ styles.buttonsContainer}>
-      <button className={styles.button}>Ввести новое</button>
-      <button className={styles.button} disabled>Добавить в список</button>
-    </div>
-    <div className={styles.listContainer}>
-      <h2 className={styles['list-he ading']}>Список:</h2>
-      <p className={styles['no-margin-text']}>Нет добавленных элементов</p>
-      <ul className={styles.list}>
-        <li className={styles['list-item']}>Первый элемент</li>
-      </ul>
-    </div>
-  </div>
-  )
-}
+	const isValueVaild = value.length >= 3;
+
+	const onAddButtonClick = () => {};
+
+	const onInputButtonClick = () => {
+		const promptValue = prompt('Введите новое значение:');
+		if (promptValue === null) return;
+		if (promptValue.length < 3) {
+			setError('Введённое значение должно содержать минимум 3 символа');
+		} else {
+			setValue(promptValue);
+			setError('');
+		}
+		console.log('Полученое новое значение', promptValue);
+		setValue(promptValue || '');
+	};
+
+	return (
+		<div className={styles.app}>
+			<h1 className={styles['page-heading']}>Ввод значения</h1>
+			<p className={styles['no-margin-text']}>
+				Текущее значение <code>value</code>: "
+				<output className={styles['current-value']}>{value}</output>"
+			</p>
+			{error && <div className={styles.error}> {error}</div>}
+			<div className={styles.buttonsContainer}>
+				<button className={styles.button} onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
+
+				<button
+					className={styles.button}
+					disabled={!isValueVaild}
+					onClick={onAddButtonClick}
+				>
+					Добавить в список
+				</button>
+			</div>
+			<div className={styles.listContainer}>
+				<h2 className={styles['list-he ading']}>Список:</h2>
+				<p className={styles['no-margin-text']}>Нет добавленных элементов</p>
+				<ul className={styles.list}>
+					<li className={styles['list-item']}>Первый элемент</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
